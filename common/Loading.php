35edb423ebed5dc202b64122de564b56e0ccb696
Mene5258 <irukashou@gmail.com>
2024-09-25 15:22:51 +0900

    ローディングページ　JS未着手

---
+++ common/Loading.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <!--====レイアウトを制御する独自のCSSを読み込み====-->
+  <link href="css/load.css" rel="stylesheet">
+  <!--====レイアウトを制御する独自のCSSを読み込み====-->
+</head>
+
+<body>
+
+  <!-- 画像埋め込み -->
+  <div class="content">
+    <img src="images/logo.jpg" alt="logo">
+  </div>
+  <!-- 文字が現れるアニメーション -->
+  <div class="txt">
+    <p>L</p>
+    <p>o</p>
+    <p>a</p>
+    <p>d</p>
+    <p>i</p>
+    <p>n</p>
+    <p>g</p>
+    <p>・</p>
+    <p>・</p>
+    <p>・</p>
+  </div>
+  <!-- ここまで -->
+
+</body>
+
+</html>
