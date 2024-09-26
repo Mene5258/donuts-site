917b721ea5e0ef5457f06edf728087af34384113
ailasunsun <airakawaguchi55@gmail.com>
2024-09-26 11:36:33 +0900

    footer/datebase(pdo接続)更新しました by川口

---
+++ includes/footer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    <footer></footer>
+  </title>
+  <link rel="stylesheet" href="../common/css/reset.css">
+  <link rel="stylesheet" href="../common/css/common.css">
+</head>
+
+<body>
+
+  <footer>
+    <div class="footer-contents">
+      <ul class="footer-links">
+        <li><a href="#">
+            よくある質問
+          </a></li>
+        <li><a href="#">
+            お問い合わせ
+          </a></li>
+        <li><a href="#">
+            当サイトのポリシー
+          </a></li>
+      </ul>
+
+      <div class="footer-icons">
+        <a href="#"><img src="../common/images/instagram.png" alt="instagram"></a>
+        <a href="#"><img src="../common/images/facebook.png" alt="facecook"></a>
+        <a href="#"><img src="../common/images/x.png" alt="x"></a>
+      </div>
+
+      <div class="footer-logo"><img src="../common/images/logo.svg" alt="logo"></div>
+
+
+      <p class="copyright">Copyright (C) c.c.donuts All Rights Reserved</p>
+    </div>
+
+  </footer>
+
+</body>
+
+</html>
