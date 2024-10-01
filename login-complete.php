9b1f6f3e876af64ed77a1eea4ebc89d2227901ff
akkineko <chibibeans@gmail.com>
2024-10-01 14:48:47 +0900

    ログイン/カート/クレジットカード/ページ基本情報追加しています　

---
+++ login-complete.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="common/css/reset.css" rel="stylesheet">
+  <link href="common/css/common.css" rel="stylesheet">
+
+  <title>login-complete</title>
+</head>
+<?php
+require '../donuts-site/includes/header.php';
+// header終了タグまで
+?>
+
+<main>
+
+
+
+</main>
+
+<?php
+//footer開始タグから
+require '../donuts-site/includes/footer.php';
+?>
