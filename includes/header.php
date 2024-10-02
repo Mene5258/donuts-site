956a9e1861cd9533c2314bb9e75a0a82860596d7
shirucci <ca18det_yasu@hotmail.com>
2024-10-02 10:32:20 +0900

    header部

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="common/css/reset.css" rel="stylesheet">
-  <link href="common/css/common.css" rel="stylesheet">
-
-  <title>donuts site</title>
-</head>
-
-<body>
   <header>
     <div class="inner">
       <ul class="header-flex">
