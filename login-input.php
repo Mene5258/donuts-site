059e4f0409e2844f7d5141a812983ba1ad0b3233
akkineko <chibibeans@gmail.com>
2024-10-02 11:46:04 +0900

    追加変更です

--- login-input.php
+++ login-input.php
@@ -6,19 +6,39 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="common/css/reset.css" rel="stylesheet">
   <link href="common/css/common.css" rel="stylesheet">
+  <link href="common/css/input.css" rel="stylesheet">
 
   <title>login-input</title>
+
+
 </head>
 <?php
 require '../donuts-site/includes/header.php';
 // header終了タグまで
 ?>
 
-<main>
+<h2>ログイン</h2>
+<div class="login-container">
+
+  <form action="#" method="POST">
+    <label for="email">メールアドレス</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="password">パスワード</label>
+    <input type="password" id="password" name="password" required>
+
+    <button class="login" type="submit">ログインする</button>
+  </form>
+
+</div>
+<p class="customar_reg">
+  <a href="#">会員登録がお済みでない方はこちら</a>
+</p>
+</body>
 
 
 
-</main>
+
 
 <?php
 //footer開始タグから
