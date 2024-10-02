eef6d496205f162998dbd98c8b59812587e6a2ab
shirucci <ca18det_yasu@hotmail.com>
2024-10-02 09:00:17 +0900

    こみっとこみっと

--- includes/header.php
+++ includes/header.php
@@ -12,29 +12,35 @@
 
 <body>
   <header>
-    <ul class="header-flex">
+    <div class="inner">
+     <ul class="header-flex">
       <li class="li-left">
-        <img src="common/images/hamburger-icon.svg" alt="hamburger-menu" class="hamburger-btn hamburger-style">
+       <img src="common/images/hamburger-icon.svg" alt="hamburger-menu" class="hamburger-btn hamburger-style">
       </li>
       <li>
-        <img src="common/images/logo.svg" alt="logo" class="logo-icon">
+       <img src="common/images/logo.svg" alt="logo" class="logo-icon">
       </li>
       <li>
-        <img src="common/images/icon-login.svg" alt="login" class="login-icon">
-        <img src="common/images/icon-cart.svg" alt="cart" class="cart-icon">
+       <img src="common/images/icon-login.svg" alt="login" class="login-icon">
+       <img src="common/images/icon-cart.svg" alt="cart" class="cart-icon">
       </li>
-    </ul>
-    </div>
+     </ul>
+     </div>
 
 
     <!-- 検索 -->
+     <div class="max-inner">
     <form action="#" method="post">
-      <input type="search" name="product">
-      <input type="submit" value="test" id="submit-btn">
-      <label for="submit-btn" class="submit-btn">
-        <img src="common/images/icon-search.svg" alt="検索">
-      </label>
+       <input type="search" name="product">
+       <input type="submit" value="test" id="submit-btn">
+       <label for="submit-btn" class="submit-btn">
+         <img src="common/images/icon-search.svg" alt="検索">
+       </label>
     </form>
+</div>
+
+
+   <!-- メニュー部 -->
     <nav class="menu-container">
       <div class="menu-logo">
         <img src="common/images/logo.svg" alt="logo">
@@ -48,6 +54,4 @@
         <li><a href="#">当サイトのポリシー</a></li>
       </ul>
     </nav>
-
-
   </header>
