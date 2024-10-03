d910d42511deeeaa9cee2ef4e77fc9c296019c0e
Mene5258 <irukashou@gmail.com>
2024-10-03 11:49:45 +0900

    どろわーPC完成　カートHTML/CSS記述中

--- cart.php
+++ cart.php
@@ -17,11 +17,23 @@ require '../donuts-site/includes/header.php';
 ?>
 
 
-<main>
+<main class="cart-page">
+  <div>
+    <img src="common/images/product-item1.jpg">
+    <p>CCドーナツ 当店オリジナル（５個入り）</p>
+    <p>税込 ￥1,500</p>
+    <p>数量 n個</p>
+    <a href="#">削除する</a>
+  </div>
 
+  <div>
+    <p>ご注文合計：￥x,xxx</p>
+    <div>
+      <input type="submit" value="ご購入確認へ進む">
+    </div>
+  </div>
 
-
-
+  <a href="#">買い物を続ける</a>
 </main>
 
 <?php
