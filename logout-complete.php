baba8b7cd669c3ed4b6f32120d6cb7d6633e5529
shirucci <ca18det_yasu@hotmail.com>
2024-10-03 14:26:00 +0900

    最終こみっとさんさん

--- logout-complete.php
+++ logout-complete.php
@@ -1,26 +1,66 @@
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="common/css/reset.css" rel="stylesheet">
-  <link href="common/css/common.css" rel="stylesheet">
-
-  <title>logout-complete</title>
+  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="common/css/common.css">
+  <link rel="stylesheet" href="common/css/logout-complete.css">
+  <title>ログアウト入力 | C.C.Donuts</title>
 </head>
 <?php
-require '../donuts-site/includes/header.php';
+require 'includes/header.php';
 // header終了タグまで
 ?>
 
 <main>
-
-
-
+  <?php
+  unset($_SESSION['customer']);
+  require 'includes/database.php';
+  $sql = $pdo->prepare('select * from customer where mail=? and password=?');
+  $sql->execute([$_REQUEST['mail'], $_REQUEST['password']]);
+  foreach ($sql as $row) {
+    $_SESSION['customer'] = [
+      'id' => $row['id'],
+      'name' => $row['name'],
+      'kana' => $row['kana'],
+      'post_code' => $row['post_code'],
+      'address' => $row['address'],
+      'mail' => $row['mail'],
+      'password' => $row['password']
+    ];
+  }
+  if (isset($_SESSION['customer'])) {
+    echo <<<END
+  <p class="user">ようこそ {$row['name']}様</p>
+  <p class="border"></p>
+  <h2>ログイン完了</h2>
+  <p class="login-box">ログインが完了しました。</p>
+  <p class="top-reverse">
+    <a href="index.php">topページへ戻る</a>
+  </p>
+END;
+  } else {
+    echo <<<END
+  <p class="user">ようこそ ゲスト様</p>
+  <p class="border"></p>
+  <h2>ログインできません</h2>
+  <p class="login-box">
+  メールアドレス<br>
+  または<br>
+  パスワードが違います
+  </p>
+  <p class="top-reverse">
+    <a href="login-input.php">ログインページへ戻る</a>
+  </p>
+END;
+  }
+  ?>
 </main>
 
 <?php
 //footer開始タグから
-require '../donuts-site/includes/footer.php';
+require 'includes/footer.php';
 ?>
--- logout-input.php
+++ logout-input.php
@@ -1,3 +1,5 @@
+<!-- セッションスタート -->
+<?php session_start() ?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -6,18 +8,39 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="common/css/reset.css" rel="stylesheet">
   <link href="common/css/common.css" rel="stylesheet">
-
-  <title>logout-input</title>
+  <link href="common/css/logout-input.css" rel="stylesheet">
+  <title>ログアウト入力 | C.C.Donuts</title>
 </head>
 <?php
-require '../donuts-site/includes/header.php';
+require 'includes/header.php';
 // header終了タグまで
 ?>
 
 <main>
-
-
-
+  <?php
+  if (isset($_SESSION['customer'])) {
+    echo <<<END
+    <p class="user">ようこそ {$_REQUEST['customer']['name']}様</p>
+    <p class="border"></p>
+    <h2>ログアウト</h2>
+    <form action="logout-complete.php" method="post">
+      <p>ログアウトしますか？</p>
+      <button class="login" type="submit">ログアウトする</button>
+    </form>
+    END;
+  } else {
+    echo <<<END
+      <p class="user">ようこそ ゲスト様</p>
+      <p class="border"></p>
+      <h2>ログアウト中です</h2>
+      <p class="login-box">
+            </p>
+      <p class="top-reverse">
+        <a href="login-input.php">ログインページへ戻る</a>
+      </p>
+      END;
+  }
+  ?>
 </main>
 
 <?php
