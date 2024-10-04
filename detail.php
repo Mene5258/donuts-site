908074a984019fe48f4cd298bff1b97a3381b54c
akkineko <chibibeans@gmail.com>
2024-10-04 11:42:34 +0900

    detaile追加

--- detail.php
+++ detail.php
@@ -8,8 +8,8 @@
   <link href="common/css/reset.css" rel="stylesheet">
   <link href="common/css/common.css" rel="stylesheet">
   <link href="common/css/detail.css" rel="stylesheet">
-
-  <title>detail</title>
+  <script src="common/js/drawer.js"></script>
+  <title>商品詳細 | C.C.Donuts</title>
 </head>
 
 <body>
@@ -18,9 +18,7 @@
   require 'includes/header.php';
   // header終了タグまで
   ?>
-
-
-  <main>
+  <main class="detaile">
 
     <!-- パンくず↓ -->
     <nav aria-label="breadcrumb">
@@ -32,21 +30,29 @@
     </nav>
 
 
-    <p class="user">ようこそ ゲスト様</p>
+    <?php
+    // セッション変数がセットされているかどうかを判定(セッション情報がちゃんと取得できているかどうかを判断)
+    if (isset($_SESSION['customer'])) {
+      // セットされていればtrue
+      echo '<p class="login-text mx">ようこそ　', $_SESSION['customer']['name'], '様</p>';
+    } else {
+      echo '<p class="login-text mx">ようこそ　ゲスト様</p>';
+    }
+    ?>
     <p class="border"></p>
 
-    <main class="cart-page">
-      <div class="cart-product">
-        <img src="common/images/product-item1.jpg">
-        <p>CCドーナツ 当店オリジナル（５個入り）</p>
-        <p class="explanation">当店のオリジナル商品、CCドーナツは、サクサクの食感が特徴のプレーンタイプのドーナツです。素材にこだわり、丁寧に揚げた生地は軽やかでサクッとした食感が楽しめます。一口食べれば、口の中に広がる甘くて香ばしい香りと、口どけの良い食感が感じられます。</p>
-        <p class="price">税込 ￥1,500 <img src="common/images/favorite.png" alt=""></p>
+
+    <div class="detaile-product">
+      <img class="detaile-main" src="common/images/product-item1.jpg">
+      <p>CCドーナツ 当店オリジナル（５個入り）</p>
+      <p class="explanation">当店のオリジナル商品、CCドーナツは、サクサクの食感が特徴のプレーンタイプのドーナツです。素材にこだわり、丁寧に揚げた生地は軽やかでサクッとした食感が楽しめます。一口食べれば、口の中に広がる甘くて香ばしい香りと、口どけの良い食感が感じられます。</p>
+      <p class="price">税込 ￥1,500 <img src="common/images/favorite.png" alt=""></p>
 
 
 
-    </main>
+  </main>
 
-    <?php
-    //footer開始タグから
-    require 'includes/footer.php';
-    ?>
+  <?php
+  //footer開始タグから
+  require 'includes/footer.php';
+  ?>
