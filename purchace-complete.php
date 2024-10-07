1177e38c79b77a580b11673fd6f61a38debae4b6
ailasunsun <airakawaguchi55@gmail.com>
2024-10-07 14:45:29 +0900

    purchace-complete/confirm update by kawaguchi

--- purchace-complete.php
+++ purchace-complete.php
@@ -12,22 +12,27 @@
   <title>ご購入完了 | C.C.Donuts</title>
 
   <main>
-  <div class="grid">
-    <p>
-      <a href="index.php">
-        <img src="common/images/logo.svg" alt="logo" class="logo">
-      </a>
-    </p>
+    <div class="grid">
+      <p>
+        <a href="index.php">
+          <img src="common/images/logo.svg" alt="logo" class="logo">
+        </a>
+      </p>
 
-    
-        <h2>ご購入完了</h2>
-        <div class="container">
+      <?php
+      // 購入したらカート空にする
+      if (isset($_SESSION['product'])) {
+        unset($_SESSION['product']);
+      }
+      ?>
+      <h2>ご購入完了</h2>
+      <div class="container">
         <p>ご購入が完了しました。</p>
-        <p>今後ともご愛顧の程、宜しくお願いいたします。</p>  
+        <p>今後ともご愛顧の程、宜しくお願いいたします。</p>
       </div>
-          <p class="return-top">
-            <a href="index.php">TOPページへ戻る</a>
-</div>
-</main>
+      <p class="return-top">
+        <a href="index.php">TOPページへ戻る</a>
+    </div>
+  </main>
 
-<?php require 'includes/footer.php';
+  <?php require 'includes/footer.php';
